566d3d9f9b60e3aef89d9d0d3aa2c702b742cf70
Simon Kolotov <simonkolotov@gmail.com>
2017-11-01 22:12:54 -0700

    Finished ex6

--- HW/ex6/dataset3Params.m
+++ HW/ex6/dataset3Params.m
@@ -8,8 +8,9 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
+#Those are the correct values for the submission
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,7 +24,40 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+cValues = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmaValues = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+cValuesNum = length(cValues);
+sigmaValuesNum = length(sigmaValues);
+minPredictionError = inf;
+
+#commented out to speed up submission
+%for i=1:cValuesNum
+%    for j=1:sigmaValuesNum
+%      
+%        printf('iteration %d/%d\n', (i-1)*cValuesNum + j, cValuesNum*sigmaValuesNum);
+%      
+%        cCurVal = cValues(i);
+%        sigmaCurVal = sigmaValues(j);
+%        
+%        model = svmTrain(X, y, cCurVal, @(x1, x2) gaussianKernel(x1, x2, sigmaCurVal)); 
+%        predictions = svmPredict(model, Xval);
+%        
+%        predictions_error = mean(double(predictions ~= yval));
+%        
+%        printf ("Prediction Error: %f\n", predictions_error);
+%        
+%        if predictions_error < minPredictionError
+%          minPredictionError = predictions_error;
+%          C = cCurVal;
+%          sigma = sigmaCurVal;
+%          printf ("Prediction Error improved, C=%.03f sigma = %.03f\n", predictions_error, C, sigma);
+%        end
+%        
+%        printf("\n\n");
+%    end
+%end
+%printf("final values: C = %f, sigma = %f", C, sigma);
 
 
 
--- HW/ex6/ex6_spam.m
+++ HW/ex6/ex6_spam.m
@@ -26,38 +26,40 @@ clear ; close all; clc
 %  complete the code in processEmail.m to produce a word indices vector
 %  for a given email.
 
-fprintf('\nPreprocessing sample email (emailSample1.txt)\n');
-
-% Extract Features
-file_contents = readFile('emailSample1.txt');
-word_indices  = processEmail(file_contents);
-
-% Print Stats
-fprintf('Word Indices: \n');
-fprintf(' %d', word_indices);
-fprintf('\n\n');
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
+if false
+  fprintf('\nPreprocessing sample email (emailSample1.txt)\n');
+  
+  % Extract Features
+  file_contents = readFile('emailSample1.txt');
+  word_indices  = processEmail(file_contents);
+  
+  % Print Stats
+  fprintf('Word Indices: \n');
+  fprintf(' %d', word_indices);
+  fprintf('\n\n');
+  
+  #fprintf('Program paused. Press enter to continue.\n');
+  #pause;
+end
 %% ==================== Part 2: Feature Extraction ====================
 %  Now, you will convert each email into a vector of features in R^n. 
 %  You should complete the code in emailFeatures.m to produce a feature
 %  vector for a given email.
-
-fprintf('\nExtracting features from sample email (emailSample1.txt)\n');
-
-% Extract Features
-file_contents = readFile('emailSample1.txt');
-word_indices  = processEmail(file_contents);
-features      = emailFeatures(word_indices);
-
-% Print Stats
-fprintf('Length of feature vector: %d\n', length(features));
-fprintf('Number of non-zero entries: %d\n', sum(features > 0));
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+if true
+  fprintf('\nExtracting features from sample email (emailSample1.txt)\n');
+  
+  % Extract Features
+  file_contents = readFile('emailSample1.txt');
+  word_indices  = processEmail(file_contents);
+  features      = emailFeatures(word_indices);
+  
+  % Print Stats
+  fprintf('Length of feature vector: %d\n', length(features));
+  fprintf('Number of non-zero entries: %d\n', sum(features > 0));
+  
+  #fprintf('Program paused. Press enter to continue.\n');
+  #pause;
+end
 
 %% =========== Part 3: Train Linear SVM for Spam Classification ========
 %  In this section, you will train a linear classifier to determine if an
@@ -66,7 +68,7 @@ pause;
 % Load the Spam Email dataset
 % You will have X, y in your environment
 load('spamTrain.mat');
-
+if true
 fprintf('\nTraining Linear SVM (Spam Classification)\n')
 fprintf('(this may take 1 to 2 minutes) ...\n')
 
@@ -76,7 +78,7 @@ model = svmTrain(X, y, C, @linearKernel);
 p = svmPredict(model, X);
 
 fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
-
+end
 %% =================== Part 4: Test Spam Classification ================
 %  After training the classifier, we can evaluate it on a test set. We have
 %  included a test set in spamTest.mat
@@ -85,12 +87,15 @@ fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
 % You will have Xtest, ytest in your environment
 load('spamTest.mat');
 
+if true
 fprintf('\nEvaluating the trained Linear SVM on a test set ...\n')
 
 p = svmPredict(model, Xtest);
 
 fprintf('Test Accuracy: %f\n', mean(double(p == ytest)) * 100);
-pause;
+#pause;
+
+end
 
 
 %% ================= Part 5: Top Predictors of Spam ====================
@@ -102,18 +107,20 @@ pause;
 %
 
 % Sort the weights and obtin the vocabulary list
-[weight, idx] = sort(model.w, 'descend');
-vocabList = getVocabList();
+if true
+  [weight, idx] = sort(model.w, 'descend');
+  vocabList = getVocabList();
 
-fprintf('\nTop predictors of spam: \n');
-for i = 1:15
-    fprintf(' %-15s (%f) \n', vocabList{idx(i)}, weight(i));
+
+  fprintf('\nTop predictors of spam: \n');
+  for i = 1:15
+      fprintf(' %-15s (%f) \n', vocabList{idx(i)}, weight(i));
+  end
+  
+  fprintf('\n\n');
+  #fprintf('\nProgram paused. Press enter to continue.\n');
+  #pause;
 end
-
-fprintf('\n\n');
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
 %% =================== Part 6: Try Your Own Emails =====================
 %  Now that you've trained the spam classifier, you can use it on your own
 %  emails! In the starter code, we have included spamSample1.txt,
@@ -125,14 +132,16 @@ pause;
 % Set the file to be read in (change this to spamSample2.txt,
 % emailSample1.txt or emailSample2.txt to see different predictions on
 % different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
+if false
+  filename = 'spamSample1.txt';
+  
+  % Read and predict
+  file_contents = readFile(filename);
+  word_indices  = processEmail(file_contents);
+  x             = emailFeatures(word_indices);
+  p = svmPredict(model, x);
+  
+  fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+  fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
 
-% Read and predict
-file_contents = readFile(filename);
-word_indices  = processEmail(file_contents);
-x             = emailFeatures(word_indices);
-p = svmPredict(model, x);
-
-fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
-
+end
